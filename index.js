bf8fd520be40b037035acd3b0b70b92bec7baced
taddiemason <137760738+taddiemason@users.noreply.github.com>
2025-06-16 14:23:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env, ctx) {
     try {
-      const githubUrl = "https://raw.githubusercontent.com/taddiemason/JoesWebsite/main/takedown.html";
+      const githubUrl = "https://raw.githubusercontent.com/taddiemason/JoesWebsite/main/index.html";
 
       const response = await fetch(githubUrl);
 
